f64815e0ab7bff27006b18efb3d568052dee2541
mohmara <mark.mohr@boystown.org>
2020-09-25 16:29:03 -0500

    fix UpdateTriggersWorker to check if lines are present before updating triggers

--- app/workers/update_triggers_worker.rb
+++ app/workers/update_triggers_worker.rb
@@ -4,19 +4,27 @@ class UpdateTriggersWorker
   def perform(game_id)
     g = Game.find game_id
     triggers = g.triggers
-    triggers.open.spread.greater_eq.where('target <= ? and triggers.team_id = ?', g.spread, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.spread.less_eq.where('target >= ? and triggers.team_id = ?', g.spread, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.spread.greater_eq.where('target <= ? and triggers.team_id = ?', g.spread * -1, g.visitor_id).map {|x| x.update(status: "triggered")}
-    triggers.open.spread.greater_eq.where('target >= ? and triggers.team_id = ?', g.spread * -1, g.visitor_id).map {|x| x.update(status: "triggered")}
-    triggers.open.total.greater_eq.where('target <= ?', g.total).map {|x| x.update(status: "triggered")}
-    triggers.open.total.less_eq.where('target >= ?', g.total).map {|x| x.update(status: "triggered")}
-    triggers.open.moneyline.greater_eq.where('target <= ? and triggers.team_id = ?', g.home_ml, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.moneyline.less_eq.where('target >= ? and triggers.team_id = ?', g.home_ml, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.moneyline.greater_eq.where('target <= ? and triggers.team_id = ?', g.visitor_ml, g.visitor_id).map {|x| x.update(status: "triggered")}
-    triggers.open.moneyline.less_eq.where('target >= ? and triggers.team_id = ?', g.visitor_ml, g.visitor_id).map {|x| x.update(status: "triggered")}
-    triggers.open.runline.greater_eq.where('target <= ? and triggers.team_id = ?', g.home_rl, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.runline.less_eq.where('target >= ? and triggers.team_id = ?', g.home_rl, g.home_id).map {|x| x.update(status: "triggered")}
-    triggers.open.runline.greater_eq.where('target <= ? and triggers.team_id = ?', g.visitor_rl, g.visitor_id).map {|x| x.update(status: "triggered")}
-    triggers.open.runline.less_eq.where('target >= ? and triggers.team_id = ?', g.visitor_rl, g.visitor_id).map {|x| x.update(status: "triggered")}
+    if g.spread.present?
+      triggers.open.spread.greater_eq.where('target <= ? and triggers.team_id = ?', g.spread, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.spread.less_eq.where('target >= ? and triggers.team_id = ?', g.spread, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.spread.greater_eq.where('target <= ? and triggers.team_id = ?', g.spread * -1, g.visitor_id).map {|x| x.update(status: "triggered")}
+      triggers.open.spread.greater_eq.where('target >= ? and triggers.team_id = ?', g.spread * -1, g.visitor_id).map {|x| x.update(status: "triggered")}
+    end
+    if g.total.present?
+      triggers.open.total.greater_eq.where('target <= ?', g.total).map {|x| x.update(status: "triggered")}
+      triggers.open.total.less_eq.where('target >= ?', g.total).map {|x| x.update(status: "triggered")}
+    end
+    if g.home_ml.present? && g.visitor_ml.present?
+      triggers.open.moneyline.greater_eq.where('target <= ? and triggers.team_id = ?', g.home_ml, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.moneyline.less_eq.where('target >= ? and triggers.team_id = ?', g.home_ml, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.moneyline.greater_eq.where('target <= ? and triggers.team_id = ?', g.visitor_ml, g.visitor_id).map {|x| x.update(status: "triggered")}
+      triggers.open.moneyline.less_eq.where('target >= ? and triggers.team_id = ?', g.visitor_ml, g.visitor_id).map {|x| x.update(status: "triggered")}
+    end
+    if g.home_rl.present? && g.visitor_rl.present?
+      triggers.open.runline.greater_eq.where('target <= ? and triggers.team_id = ?', g.home_rl, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.runline.less_eq.where('target >= ? and triggers.team_id = ?', g.home_rl, g.home_id).map {|x| x.update(status: "triggered")}
+      triggers.open.runline.greater_eq.where('target <= ? and triggers.team_id = ?', g.visitor_rl, g.visitor_id).map {|x| x.update(status: "triggered")}
+      triggers.open.runline.less_eq.where('target >= ? and triggers.team_id = ?', g.visitor_rl, g.visitor_id).map {|x| x.update(status: "triggered")}
+    end
   end
 end
