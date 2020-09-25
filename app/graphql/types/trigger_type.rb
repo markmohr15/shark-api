69f57bd8e5bd9ee04a409dba2817176743abd445
mohmara <mark.mohr@boystown.org>
2020-09-25 00:34:09 -0500

    add timestamp to Trigger Type

--- app/graphql/types/trigger_type.rb
+++ app/graphql/types/trigger_type.rb
@@ -10,5 +10,6 @@ module Types
     field :team, Types::TeamType, null: true
     field :user, Types::UserType, null: false
     field :display_target, String, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
