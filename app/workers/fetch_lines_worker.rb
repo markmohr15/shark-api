aed64dbb9f4bd4e1116e47bd94942f16895f47b5
mohmara <mark.mohr@boystown.org>
2020-09-29 09:55:52 -0500

    catch errors and keep going with FetchLinesWorker

--- app/workers/fetch_lines_worker.rb
+++ app/workers/fetch_lines_worker.rb
@@ -2,12 +2,16 @@ class FetchLinesWorker
   include Sidekiq::Worker
 
   def perform
-    BetOnlineLines::Cfb.get_lines
-    BetOnlineLines::Kbo.get_lines
-    BetOnlineLines::Mlb.get_lines
-    BetOnlineLines::Nba.get_lines
-    BetOnlineLines::Nfl.get_lines
-    BetOnlineLines::Nhl.get_lines
-    BetOnlineLines::Npb.get_lines
+    begin
+      BetOnlineLines::Cfb.get_lines
+      BetOnlineLines::Kbo.get_lines
+      BetOnlineLines::Mlb.get_lines
+      BetOnlineLines::Nba.get_lines
+      BetOnlineLines::Nfl.get_lines
+      BetOnlineLines::Nhl.get_lines
+      BetOnlineLines::Npb.get_lines
+    rescue => err 
+      Sidekiq.logger.info err
+    end
   end
 end
