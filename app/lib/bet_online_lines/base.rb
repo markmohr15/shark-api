916a485613fe6c34c83708ab85a24ec4da438aba
mohmara <mark.mohr@boystown.org>
2020-10-07 00:11:02 -0500

    bugsnag setup pt. 2

--- app/lib/bet_online_lines/base.rb
+++ app/lib/bet_online_lines/base.rb
@@ -1,7 +1,9 @@
 class BetOnlineLines::Base
 
-  def self.raise_api_error message
-    Bugsnag.notify message
+  def self.raise_api_error err
+    console.log(err)
+    Sidekiq.logger.info err
+    Bugsnag.notify err
     nil
   end
 
--- app/workers/fetch_lines_worker.rb
+++ app/workers/fetch_lines_worker.rb
@@ -6,36 +6,43 @@ class FetchLinesWorker
       BetOnlineLines::Cfb.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Mlb.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Nfl.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Kbo.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Npb.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Nba.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
     begin
       BetOnlineLines::Nhl.get_lines
     rescue => err
       Sidekiq.logger.info err
+      Bugsnag.notify(err)
     end
   end
 end
