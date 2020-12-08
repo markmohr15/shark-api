3a240b87d1a666eab0b57c32cf49987655a6cc28
mohmara <mark.mohr@boystown.org>
2020-12-08 15:03:41 -0600

    fix BOL CFB bug

--- app/lib/bet_online_lines/cfb.rb
+++ app/lib/bet_online_lines/cfb.rb
@@ -9,7 +9,8 @@ class BetOnlineLines::Cfb < BetOnlineLines::Base
   end
 
   def self.team name
-    sport.teams.find_by_name(name.join(" ")) || sport.teams.find_by_name(name[0..1].join(" ")) || sport.teams.find_by_name(name[0..2].join(" "))
+    sport.teams.find_by_name(name.join(" ")) || sport.teams.find_by_name(name[0..1].join(" ")) || 
+    sport.teams.find_by_name(name[0..2].join(" ")) || sport.teams.find_by_name(name[0])
   end
   
   def self.get_lines
@@ -18,6 +19,7 @@ class BetOnlineLines::Cfb < BetOnlineLines::Base
     return if base_dates.empty?
     date = dates[0][0][0].split(" -")[0].to_date
     date2 = dates[dates.size - 1][0][0].split(" -")[0].to_date
+    @nf = []
     games.each do |g|
       next if g[0][0].blank?
       game_info = game_info g
@@ -25,9 +27,13 @@ class BetOnlineLines::Cfb < BetOnlineLines::Base
       game = Game.Scheduled.where('sport_id = ? and gametime > ? and gametime < ? and home_id = ? and visitor_id = ?', 
                          sport.id, date.to_datetime, date2.to_datetime.end_of_day + 6.hours, 
                          team(game_info[:home_name])&.id, team(game_info[:vis_name])&.id).first
-      next if game.nil?
-      create_line game_info, game
+      if game.nil?
+        @nf << game_info[:home_name]
+      else
+        create_line game_info, game
+      end
     end
+    @nf
   rescue StandardError => exception
     raise_api_error exception.message
   end
