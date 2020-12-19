b9b6046196b7aaf1ffe1d8c738eebc01272c3a5f
mohmara <mark.mohr@boystown.org>
2020-12-19 00:19:49 -0600

    cbb 12-20-20

--- app/lib/bet_online_lines/nba.rb
+++ app/lib/bet_online_lines/nba.rb
@@ -19,6 +19,7 @@ class BetOnlineLines::Nba < BetOnlineLines::Base
     return if base_dates.empty?
     date = dates[0][0][0].split(" -")[0].to_date
     date2 = dates[dates.size - 1][0][0].split(" -")[0].to_date
+    @nf = []
     games.each do |g|
       next if g[0][0].blank?
       game_info = game_info g
@@ -26,9 +27,13 @@ class BetOnlineLines::Nba < BetOnlineLines::Base
       game = Game.Scheduled.where('sport_id = ? and gametime > ? and gametime < ? and visitor_id = ? and home_id = ?', 
                          sport.id, date.to_datetime, date2.to_datetime.end_of_day + 6.hours, 
                          team(game_info[:vis_name])&.id, team(game_info[:home_name])&.id).first
-      next if game.nil?
-      create_line game_info, game
+      if game.nil?
+        @nf << g
+      else
+        create_line game_info, game
+      end
     end
+    @nf
   rescue StandardError => exception
     raise_api_error exception.message
   end
