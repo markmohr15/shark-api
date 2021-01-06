8e85ce3e2eb60810767d0f0b9b80c93ed2302e9d
mohmara <mark.mohr@boystown.org>
2021-01-06 00:59:46 -0600

    show not found for BOL NHL

--- app/lib/bet_online_lines/nhl.rb
+++ app/lib/bet_online_lines/nhl.rb
@@ -18,16 +18,23 @@ class BetOnlineLines::Nhl < BetOnlineLines::Base
     return if base_dates.empty?
     date = dates[0][0][0].split(" -")[0].to_date
     date2 = dates[dates.size - 1][0][0].split(" -")[0].to_date
+    @nf = []
+    @found = []
     games.each do |g|
       next if g[0][0].blank?
       game_info = game_info g
       next if game_info[:vis_lines].empty? || game_info[:home_lines].empty?
-      game = Game.Scheduled.where('sport_id = ? and gametime > ? and gametime < ? and visitor_id = ? and home_id = ?', 
+      game = Game.where.not(id: @found).Scheduled.where('sport_id = ? and gametime > ? and gametime < ? and visitor_id = ? and home_id = ?', 
                          sport.id, date.to_datetime, date2.to_datetime.end_of_day + 6.hours, 
                          team(game_info[:vis_name])&.id, team(game_info[:home_name])&.id).first
-      next if game.nil?
-      create_line game_info, game
+      if game.nil?
+        @nf << game_info[:home_name]
+      else
+        create_line game_info, game
+        @found << game.id
+      end
     end
+    @nf
   rescue StandardError => exception
     raise_api_error exception.message
   end
