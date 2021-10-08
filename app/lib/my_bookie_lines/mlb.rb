1cf2b08a8202b2eedc3ef5faa0c390c7947efbd8
mohmara <mark.mohr@boystown.org>
2021-10-08 16:44:09 -0500

    let base MyBookie do MLB

--- app/lib/my_bookie_lines/mlb.rb
+++ app/lib/my_bookie_lines/mlb.rb
@@ -13,28 +13,4 @@ class MyBookieLines::Mlb < MyBookieLines::Base
     sport.tags.find_by_name(name[0..1])&.team || sport.tags.find_by_name(name[0..2])&.team
   end
 
-  def self.get_lines
-    @url = @fetch = @games = nil
-    @nf = []
-    @found = []
-
-    games.each do |g|
-      game_info = game_info g
-      next if game_info[:next]
-      game = sport.games.Scheduled.where.not(id: @found).where('home_id = ? and visitor_id = ? 
-                                                                and gametime > ? and gametime < ?', 
-                team(game_info[:home_name])&.id, team(game_info[:vis_name])&.id, game_info[:gametime] - 90.minutes,
-                game_info[:gametime] + 90.minutes).first
-      if game.nil?
-        @nf << [game_info[:vis_name], game_info[:home_name]]
-      else
-        create_line game_info, game
-        @found << game.id
-      end
-    end
-    @nf
-  rescue StandardError => exception
-    raise_api_error exception.message
-  end
-
 end
