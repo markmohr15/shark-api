da7ece0708588b6e3085a7adc3057ee6321048cf
mohmara <mark.mohr@boystown.org>
2021-10-08 16:55:17 -0500

    MyBookie case insensitive tag fix

--- app/lib/my_bookie_lines/base.rb
+++ app/lib/my_bookie_lines/base.rb
@@ -23,7 +23,7 @@ class MyBookieLines::Base
   end
 
   def self.team name
-    sport.tags.find_by_name(name)&.team
+    sport.tags.where("lower(tags.name) = ?", name.downcase)&.first&.team
   end
 
   def self.get_lines
