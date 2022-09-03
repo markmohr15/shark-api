0aadbe947c9335a5607e3b70d6bd10febca03e4e
Mark Mohr <mark.mohr@improving.com>
2022-09-03 01:21:19 -0500

    fix nil bug on DeleteWeathersWorker

--- app/workers/delete_weathers_worker.rb
+++ app/workers/delete_weathers_worker.rb
@@ -3,7 +3,7 @@ class DeleteWeathersWorker
 
   def perform game_id
     game = Game.find game_id
-    gametime_weather = game.weathers.current.order(:created_at).last.id
+    gametime_weather = game.weathers.current.order(:created_at).last&.id
     game.weathers.where.not(id: gametime_weather).delete_all
   end
 end
