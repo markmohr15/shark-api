74e1a69d6caeafaba5ebc634cc9c03637af56c59
Mark Mohr <mark.mohr@improving.com>
2024-04-03 14:58:13 -0500

    cbb BOL line fix pt. 2

--- app/lib/bet_online_lines/cbb.rb
+++ app/lib/bet_online_lines/cbb.rb
@@ -8,13 +8,15 @@ class BetOnlineLines::Cbb < BetOnlineLines::Base
     @sport ||= Sport.cbb
   end
 
-  def self.team name
-    sport.tags.find_by_name(name.join(" "))&.team
+  def self.home home_name
+    sport.tags.find_by_name(home_name.join(" "))&.team
   end
   
   def self.get_lines
     @url = @fetch = @base_dates = @base_games = @dates = @games = nil
     @nf = []
+    @found = []
+
     return if base_dates.empty?
     date = dates[0][0][0].split(" -")[0].to_date
     date2 = dates[dates.size - 1][0][0].split(" -")[0].to_date
@@ -22,13 +24,16 @@ class BetOnlineLines::Cbb < BetOnlineLines::Base
       next if g[0][0].blank?
       game_info = game_info g
       next if game_info[:vis_lines].empty? || game_info[:home_lines].empty?
-      game = Game.Scheduled.where('sport_id = ? and gametime > ? and gametime < ? and home_id = ? and visitor_id = ?', 
+      game = Game.Scheduled
+                 .where.not(id: @found)
+                 .where('sport_id = ? and gametime > ? and gametime < ? and home_id = ?', 
                          sport.id, date.to_datetime, date2.to_datetime.end_of_day + 6.hours, 
-                         team(game_info[:home_name])&.id, team(game_info[:vis_name])&.id).first
+                         home(game_info[:home_name])&.id).first
       if game.nil?
         @nf << g
       else
         create_line game_info, game
+        @found << game.id
       end
     end
     @nf
